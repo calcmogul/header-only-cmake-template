3e65e20e8ce9bfae12a6719fe85e9f4801314606
Tyler Veness <calcmogul@gmail.com>
2025-11-17 09:59:33 -0800

    Initial commit

---
+++ cmake/CompilerFlags.cmake
@@ -0,0 +1,33 @@
+macro(compiler_flags target)
+    if(NOT MSVC)
+        target_compile_options(${target} PRIVATE -Wall -Wextra -pedantic)
+    else()
+        # Suppress the following warnings:
+        #   * C4244: lossy conversion
+        #   * C4251: missing dllexport/dllimport attribute on data member
+        target_compile_options(${target} PRIVATE /wd4244 /wd4251)
+    endif()
+    set_property(TARGET ${target} PROPERTY COMPILE_WARNING_AS_ERROR ON)
+
+    target_compile_features(${target} PUBLIC cxx_std_23)
+    if(MSVC)
+        target_compile_options(${target} PUBLIC /MP /Zf /utf-8 /bigobj)
+    endif()
+endmacro()
+
+macro(interface_compiler_flags target)
+    if(NOT MSVC)
+        target_compile_options(${target} INTERFACE -Wall -Wextra -pedantic)
+    else()
+        # Suppress the following warnings:
+        #   * C4244: lossy conversion
+        #   * C4251: missing dllexport/dllimport attribute on data member
+        target_compile_options(${target} INTERFACE /wd4244 /wd4251)
+    endif()
+    set_property(TARGET ${target} PROPERTY COMPILE_WARNING_AS_ERROR ON)
+
+    target_compile_features(${target} INTERFACE cxx_std_23)
+    if(MSVC)
+        target_compile_options(${target} INTERFACE /MP /Zf /utf-8 /bigobj)
+    endif()
+endmacro()
---
+++ examples/example/src/example.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) template contributors
+
+#include "example.hpp"
+
+#include <template/template.hpp>
+
+#ifndef RUNNING_TESTS
+int main() {}
+#endif
